ba22926b189bd601d23cd5d067eb6c4a151dd90c
beuoon <beuoon@naver.com>
2021-03-28 16:39:34 +0900

    Add: 메인 목표, 서브 목표 구현

--- Assets/Resources/Script/PlayScene/Goal/GoalMgr.cs
+++ Assets/Resources/Script/PlayScene/Goal/GoalMgr.cs
@@ -4,63 +4,87 @@ using System.Xml;
 using UnityEngine;
 
 public class GoalMgr {
-    private List<Goal> goals = new List<Goal>();
+    private List<Goal> mainGoals = new List<Goal>();
+    private List<Goal> subGoals = new List<Goal>();
     private G_Deadline deadline = null;
     private G_RescueSurvivor rescueSurvivor = null;
     private G_Extinguish extinguish = null;
     private G_Arrive arrive = null;
     private G_RescueImportantSurvivor rescueImportantSurvivor = null;
 
-    private GameObject GoalTextPrefab = Resources.Load<GameObject>("Prefabs/GoalText");
+    private GameObject GoalTextPrefab = Resources.Load<GameObject>("Prefabs/StageGoal");
     private GameObject StageGoals = GameObject.Find("UICanvas/PlayCanvas/TopLeftUI/StageGoals");
 
     public GoalMgr(XmlNode goalsNode) {
-        Vector3 relativePos = StageGoals.transform.position - StageGoals.transform.parent.position;
+        // Load Main Goal
+        XmlNodeList mainGoalNodes = goalsNode.SelectNodes("Main/Goal");
+        foreach (XmlNode goalNode in mainGoalNodes) {
+            GameObject textObject = GameObject.Instantiate(GoalTextPrefab, StageGoals.transform);
+            textObject.transform.localPosition = new Vector3(0, -17 + -22.5f + -45f*mainGoals.Count, 0);
 
-        // Load XML
-        XmlNodeList goalNodes = goalsNode.SelectNodes("Goal");
-        foreach (XmlNode goalNode in goalNodes) {
-            Goal.GoalType type = Goal.StringToType(goalNode.SelectSingleNode("Type").InnerText);
-
-            GameObject textObject = GameObject.Instantiate(GoalTextPrefab, StageGoals.transform.position, Quaternion.identity, StageGoals.transform.parent);
-			textObject.transform.Translate(0, -22.5f*goals.Count, 0);
-
-			Goal goalObject = null;
-			switch (type) {
-            case Goal.GoalType.DEADLINE:
-                deadline = new G_Deadline(textObject, goalNode);
-                goalObject = deadline;
-                break;
-            case Goal.GoalType.RESCUE_SURVIVOR:
-                rescueSurvivor = new G_RescueSurvivor(textObject, goalNode);
-                goalObject = rescueSurvivor;
-                break;
-            case Goal.GoalType.EXTINGUISH:
-                extinguish = new G_Extinguish(textObject, goalNode);
-                goalObject = extinguish;
-                break;
-            case Goal.GoalType.ARRIVE:
-                arrive = new G_Arrive(textObject, goalNode);
-                goalObject = arrive;
-                break;
-            case Goal.GoalType.RESCUE_IMPORTANT_SURVIVOR:
-                rescueImportantSurvivor = new G_RescueImportantSurvivor(textObject, goalNode);
-                goalObject = rescueSurvivor;
-                break;
-            }
-
-            goals.Add(goalObject);
+            Goal goal = CreateGoal(textObject, goalNode);
+            
+            mainGoals.Add(goal);
         }
 
-		float oh = StageGoals.GetComponent<RectTransform>().rect.height/2;
-		StageGoals.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 45*(goals.Count+1));
-		float ch = StageGoals.GetComponent<RectTransform>().rect.height/4;
-		StageGoals.transform.Translate(0, oh - ch, 0);
-	}
+        // Load Sub Goal
+        XmlNodeList subGoalNodes = goalsNode.SelectNodes("Sub/Goal");
+        GameObject subStart = StageGoals.transform.Find("SubStart").gameObject;
+        GameObject end = StageGoals.transform.Find("End").gameObject;
+        float subStartY = -17 + -45f*mainGoals.Count;
+
+        if (subGoalNodes.Count == 0) {
+            subStart.SetActive(false);
+            end.transform.localPosition = new Vector3(0, subStartY + -19.5f, 0);
+        }
+        else {
+            subStart.transform.localPosition = new Vector3(0, subStartY + -17, 0);
+
+            foreach (XmlNode goalNode in subGoalNodes) {
+                GameObject textObject = GameObject.Instantiate(GoalTextPrefab, StageGoals.transform);
+                textObject.transform.localPosition = new Vector3(0, subStartY + -34 + -22.5f + -45f*subGoals.Count, 0);
+
+                Goal goal = CreateGoal(textObject, goalNode);
+
+                subGoals.Add(goal);
+            }
+
+            end.transform.localPosition = new Vector3(0, subStartY + -34 + -45f*subGoals.Count + -19.5f, 0);
+        }
+    }
+    private Goal CreateGoal(GameObject textObject, XmlNode goalNode) {
+        Goal.GoalType type = Goal.StringToType(goalNode.SelectSingleNode("Type").InnerText);
+
+        Goal goalObject = null;
+        switch (type) {
+        case Goal.GoalType.DEADLINE:
+            deadline = new G_Deadline(textObject, goalNode);
+            goalObject = deadline;
+            break;
+        case Goal.GoalType.RESCUE_SURVIVOR:
+            rescueSurvivor = new G_RescueSurvivor(textObject, goalNode);
+            goalObject = rescueSurvivor;
+            break;
+        case Goal.GoalType.EXTINGUISH:
+            extinguish = new G_Extinguish(textObject, goalNode);
+            goalObject = extinguish;
+            break;
+        case Goal.GoalType.ARRIVE:
+            arrive = new G_Arrive(textObject, goalNode);
+            goalObject = arrive;
+            break;
+        case Goal.GoalType.RESCUE_IMPORTANT_SURVIVOR:
+            rescueImportantSurvivor = new G_RescueImportantSurvivor(textObject, goalNode);
+            goalObject = rescueSurvivor;
+            break;
+        }
+
+        return goalObject;
+    }
 
     public bool IsAllSatisfied() {
         bool isSatisfied = true;
-        foreach (Goal goal in goals) {
+        foreach (Goal goal in mainGoals) {
             if (!goal.IsSatisfied()) {
                 isSatisfied = false;
                 break;
@@ -70,7 +94,7 @@ public class GoalMgr {
 	}
     public bool IsImpossible() {
         bool isImpossible = false;
-        foreach (Goal goal in goals) {
+        foreach (Goal goal in mainGoals) {
             if (goal.IsImpossible()) {
                 isImpossible = true;
                 break;

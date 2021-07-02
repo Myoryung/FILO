b5e31d2b966f485d1ed99b9f1168b041cdf80493
Myoryung <iove_v@naver.com>
2021-07-02 21:23:35 +0900

    Add: 스테이지 2 타일 추가 및 애니메이션 적용

--- Assets/Resources/Script/PlayScene/Charactor/Player.cs
+++ Assets/Resources/Script/PlayScene/Charactor/Player.cs
@@ -322,7 +322,6 @@ public class Player : Charactor
         {
         case 1: // FireExtinguisher
             StartCoroutine(UseFireExtinguisher());
-            
             break;
         case 2: // StickyBomb
             StartCoroutine(UseStickyBomb());
